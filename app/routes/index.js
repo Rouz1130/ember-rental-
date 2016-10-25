696e86f60da40bcff50278e32b00588d1da3bdcc
rouz majlessi <rmajlessi@gmail.com>
2016-10-25 15:24:07 -0700

    add new and update rental files

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,26 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
-});
 
+  actions: {
+    saveRental3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+
+    update(rental, params) {
+      debugger;
+      rental.save();
+      this.transitionTo('index');
+    },
+
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
+});
 //store refers to Firebase data store , findAll method has the argument rental tells ember to find all recrods
 //in the store.
 
