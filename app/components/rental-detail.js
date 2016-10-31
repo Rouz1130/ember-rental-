a23894a7c09ebe2e8b68fe6aaf92908d73bbe88d
rouz majlessi <rmajlessi@gmail.com>
2016-10-31 11:28:39 -0700

    add computed property too sort in rental-detail component file/js

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //computed properties to sort by reviews
+  sortBy: ['rating:desc'],
+  sortedReviews: Ember.computed.sort('rental.reviews', 'sortBy'),
+
   actions: {
     delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
